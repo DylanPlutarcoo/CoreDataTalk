5d0d8b998130c196c962e704f8a2d5a9f4d3f58d
Luca <lucapaiva84@gmail.com>
2023-08-22 19:08:25 -0300

    ATT

---
+++ CoreDataTest/ItemTask.swift
@@ -0,0 +1,15 @@
+//
+//  ItemTask.swift
+//  CoreDataTest
+//
+//  Created by userext on 21/08/23.
+//
+
+import Foundation
+import CoreData
+
+
+//public class TaskItem: NSManagedObject {
+//    @NSManaged public var title: String
+//    @NSManaged public var isCompleted: Bool
+//}
---
+++ CoreDataTest/TaskView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import CoreData
+
+struct TaskView: View {
+    @StateObject var viewModel = TaskViewModel()
+    @State private var newTask = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Insira a tarefa", text: $newTask, onCommit: {
+                    viewModel.addTask(title: newTask)
+                    newTask = ""
+                })
+                .padding(.horizontal, 20)
+                List {
+                    ForEach(viewModel.savedTasks) { task in
+                        HStack {
+                            Button(action: {
+                                viewModel.toggleTaskCompletion(task: task)
+                            }) {
+                                Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+                            }
+                            Text(task.title ?? "")
+                                .strikethrough(task.isCompleted)
+                        }
+                    }
+                    .onDelete(perform: viewModel.deleteTasks)
+                }
+                .navigationBarTitle("All Tasks")
+            }
+        }
+        
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView(viewModel: TaskViewModel())
+    }
+}
