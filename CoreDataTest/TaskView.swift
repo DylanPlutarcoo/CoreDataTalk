07852e9d7fbf7179008a5b6cbacf1b58d65cac59
Luca <lucapaiva84@gmail.com>
2023-08-24 21:45:57 -0300

    Nova Att

--- CoreDataTest/TaskView.swift
+++ CoreDataTest/TaskView.swift
@@ -4,38 +4,47 @@ import CoreData
 struct TaskView: View {
     @StateObject var viewModel = TaskViewModel()
     @State private var newTask = ""
+//    @FetchRequest(
+//            sortDescriptors: [NSSortDescriptor(keyPath: \TaskEntity.title, ascending: true)],
+//            animation: .default)
+//        private var savedTasks: FetchedResults<TaskEntity>
     
     var body: some View {
-        NavigationView {
-            VStack {
-                TextField("Insira a tarefa", text: $newTask, onCommit: {
-                    viewModel.addTask(title: newTask)
-                    newTask = ""
-                })
-                .padding(.horizontal, 20)
-                .padding(.top, 30)
-                
-                List {
-                    ForEach(viewModel.savedTasks) { task in
-                        HStack {
-                            Button(action: {
-                                viewModel.toggleTask(task: task)
-                            }) {
-                                Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+            NavigationView {
+                VStack {
+                    TextField("Insira a tarefa", text: $newTask, onCommit: {
+                        viewModel.addTask(title: newTask)
+                        newTask = ""
+                    })
+                    .padding(.horizontal, 20)
+                    .padding(.top, 30)
+                    
+                    List {
+                        ForEach(viewModel.savedTasks) { task in
+                            HStack {
+                                Text(task.title ?? "")
+                                Spacer()
+                                
+                                NavigationLink(
+                                    destination: EditTaskView(task: task, action: {
+                                        viewModel.fetchTasks()
+                                    }),
+                                    label: {
+                                    }
+                                )
                             }
-                            Text(task.title ?? "")
-                                .strikethrough(task.isCompleted)
                         }
+                        .onDelete(perform: viewModel.deleteTasks)
                     }
-                    .onDelete(perform: viewModel.deleteTasks)
+                    .listStyle(PlainListStyle())
+                    .navigationBarTitle("All Tasks")
+                    .padding(.top, 40)
                 }
-                .listStyle(PlainListStyle())
-                .navigationBarTitle("All Tasks")
-                .padding(.top, 40)
             }
         }
+    
     }
-}
+
 
 struct TaskView_Previews: PreviewProvider {
     static var previews: some View {
